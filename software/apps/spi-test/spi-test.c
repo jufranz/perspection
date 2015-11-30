8b3519d29d9e68fc31a9c039dfe2a7c15f168646
john collins <johncollins@0587362614.wireless.umich.net>
2015-11-29 21:54:31 -0500

    spi-test works

--- software/apps/spi-test/spi-test.c
+++ software/apps/spi-test/spi-test.c
@@ -1,7 +1,8 @@
 #include "contiki.h"
 #include "random.h"
-#include "dev/spi.h"
 #include "spi-arch.h"
+#include "dev/spi.h"
+#include "gpio.h"
 
 #include <stdio.h>
 /*---------------------------------------------------------------------------*/
@@ -19,15 +20,18 @@ PROCESS_THREAD(spi_test_process, ev, data)
   //spi-initialization
   spi_init();
   spix_set_mode(0, SSI_CR0_FRF_MOTOROLA, 0, 0, 16);
+  spix_cs_init(GPIO_B_NUM , 4);
+  
 
   static uint16_t send = 105;
 
   while(1) { 
     /* Delay 1 second */
-    etimer_set(&et, CLOCK_SECOND * 1);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
+    //etimer_set(&et, CLOCK_SECOND * 1);
+    //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+    SPI_CS_CLR(GPIO_B_NUM, 4);
     SPI_WRITE(send);
+    SPI_CS_SET(GPIO_B_NUM, 4);
   }
 
   PROCESS_END();
