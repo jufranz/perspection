d9aaac216d4314e1cedb2fcdbc0463e8bf122ae3
Ben Oztalay <boztalay@gmail.com>
2015-12-02 22:14:18 -0500

    Atum SPI stuff

--- software/apps/spi-test/spi-test.c
+++ software/apps/spi-test/spi-test.c
@@ -13,19 +13,16 @@ PROCESS_THREAD(spi_test_process, ev, data)
 {
   static struct etimer et;
 
-  //PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
-
   PROCESS_BEGIN();
 
   //spi-initialization
   spi_init();
   spix_set_mode(0, SSI_CR0_FRF_MOTOROLA, 0, 0, 16);
   spix_cs_init(GPIO_B_NUM , 4);
-  
 
   static uint16_t send = 105;
 
-  while(1) { 
+  while(1) {
     /* Delay 1 second */
     //etimer_set(&et, CLOCK_SECOND * 1);
     //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
---
+++ software/platform/perspection/dev/comms.h
@@ -0,0 +1,12 @@
+#ifndef _COMMS_H_
+#define _COMMS_H_
+
+struct robotIMUData{
+    uint16_t dataYaw;
+    uint16_t dataPitch;
+};
+
+
+
+
+#endif
