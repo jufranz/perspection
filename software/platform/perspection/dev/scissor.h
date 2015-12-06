0aec5dfa1a5c211c461f5b5b5dd171b30d1288dd
godofchipotle <mikechoe12@gmail.com>
2015-12-05 20:59:36 -0500

    Create scissor.h

---
+++ software/platform/perspection/dev/scissor.h
@@ -0,0 +1,20 @@
+#include "contiki.h"
+#include "net/rime/rime.h"
+#include "random.h"
+#include "clock.h"
+#include "dev/comms.h"
+#include "dev/leds.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define PWM_FREQUENCY 20000.0
+#define PWM_PERIOD_MS (1000.0 / PWM_FREQUENCY)
+
+//DIR(PHASE pin of DRV8801) connected to Atum B4
+//PWM(ENABLE pin of DRV8801) connected to Atum B3
+
+void set_up_single_driver_gpio(uint32_t gpio_port_base, uint32_t gpio_pin_num);
+void set_up_pwm();
+void set_duty_cycle(double new_duty_cycle);
+void scissorMotorControl(double direction, double speed);
