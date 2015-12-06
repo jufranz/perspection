3b5301f34d6a23dce9d3da086ca7fe85a8f75e80
Ben Oztalay <boztalay@gmail.com>
2015-12-06 13:37:01 -0500

    Cleaned up the robot-code, just formatting stuff

--- software/apps/robot-code/body.c
+++ software/apps/robot-code/body.c
@@ -1,147 +1,103 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         Testing the broadcast layer in Rime
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
+// Includes
 
 #include "contiki.h"
+#include "clock.h"
 #include "net/rime/rime.h"
 #include "random.h"
 
-#include "clock.h"
-
 #include "dev/comms.h"
-//#include "dev/scissor.h"
 #include "dev/leds.h"
 #include "dev/spi_wrapper.h"
 
 #include <stdio.h>
 
+// Defines
+
 #define BODY_MAIN_DEBUG 0
 
-/*---------------------------------------------------------------------------*/
+// Contiki process declarations
+
 PROCESS(init_linkaddr_process, "Initialize linkaddr Address");
 PROCESS(init_network_process, "Initialize Broadcast Channel");
 AUTOSTART_PROCESSES(&init_linkaddr_process);
-/*---------------------------------------------------------------------------*/
 
+// Receiving movement commands from the controller
 
-/*---------------------------------------------------------------------------*/
 static struct moveData_t recvMoveData;
-static void
-movement_recv(struct broadcast_conn *c, const linkaddr_t *from)
-{
-  leds_on(LEDS_RED | LEDS_GREEN);
+static void movement_recv(struct broadcast_conn* c, const linkaddr_t* from) {
+    leds_on(LEDS_RED | LEDS_GREEN);
 
-  unpackMoveData(&recvMoveData);
-  //scissorMotorControl(recvMoveData.sDir, recvMoveData.sSpeed);
-  
-  #if BODY_MAIN_DEBUG
-  printf("Dir: %d, Speed: %d, Pos: %d\r\n", 
-        recvMoveData.tDir, recvMoveData.tSpeed, recvMoveData.rAngle);
-  #endif
-  
-  //sending the data off to the C2000 to do further processing
-  spi_wrapper_send_body_control(recvMoveData.tDir, recvMoveData.tSpeed);
-  spi_wrapper_send_gimbal_pos(recvMoveData.rAngle);
+    unpackMoveData(&recvMoveData);
 
-  leds_off(LEDS_RED | LEDS_GREEN);
+#if BODY_MAIN_DEBUG
+    printf("Dir: %d, Speed: %d, Pos: %d\r\n", recvMoveData.tDir, recvMoveData.tSpeed, recvMoveData.rAngle);
+#endif
+
+    // Sending the command off to the C2000
+    spi_wrapper_send_body_control(recvMoveData.tDir, recvMoveData.tSpeed);
+
+    leds_off(LEDS_RED | LEDS_GREEN);
 }
-/*---------------------------------------------------------------------------*/
+
+// Receiving gimbal position commands from the headset
+
 static struct gimbalData_t recvGimbalData;
-static void
-gimbal_recv(struct broadcast_conn *c, const linkaddr_t *from)
-{
-  leds_on(LEDS_GREEN | LEDS_BLUE);
+static void gimbal_recv(struct broadcast_conn* c, const linkaddr_t* from) {
+    leds_on(LEDS_GREEN | LEDS_BLUE);
 
-  unpackGimbalData(&recvGimbalData);
+    unpackGimbalData(&recvGimbalData);
 
-  #if BODY_MAIN_DEBUG
-  printf("360 scaled to 5760 -- Yaw: %d, Pitch: %d\n", 
-        recvGimbalData.gYaw, recvGimbalData.gPitch);  
-  #endif
+#if BODY_MAIN_DEBUG
+    printf("360 scaled to 5760 -- Yaw: %d, Pitch: %d\n", recvGimbalData.gYaw, recvGimbalData.gPitch);
+#endif
 
-  //do magic shit
+    // TODO: Magic shit
 
-  leds_off(LEDS_GREEN | LEDS_BLUE);
+    leds_off(LEDS_GREEN | LEDS_BLUE);
 }
-/*---------------------------------------------------------------------------*/
-static void
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
-{
-  if(from->u8[0] == HEADSET_ADDR_A && from->u8[1] == HEADSET_ADDR_B){
-    gimbal_recv(c, from);
-    return;
-  }
-  if(from->u8[0] == CTRL_ADDR_A && from->u8[1] == CTRL_ADDR_B){
-    movement_recv(c, from);
-    return;
-  }
+
+// Receiving packets and sending them off for processing
+
+static void broadcast_recv(struct broadcast_conn* c, const linkaddr_t* from) {
+    if(from->u8[0] == HEADSET_ADDR_A && from->u8[1] == HEADSET_ADDR_B){
+        gimbal_recv(c, from);
+        return;
+    }
+    if(from->u8[0] == CTRL_ADDR_A && from->u8[1] == CTRL_ADDR_B){
+        movement_recv(c, from);
+        return;
+    }
 }
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
 static struct broadcast_conn broadcast;
-/*---------------------------------------------------------------------------*/
 
+// Link address init process
 
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(init_linkaddr_process, ev, data)
-{
-  PROCESS_BEGIN();
+PROCESS_THREAD(init_linkaddr_process, ev, data) {
+    PROCESS_BEGIN();
 
-  spi_wrapper_init();
+    spi_wrapper_init();
 
-  static linkaddr_t nodeAddr;
-  nodeAddr.u8[0] = BODY_ADDR_A;
-  nodeAddr.u8[1] = BODY_ADDR_B;
-  linkaddr_set_node_addr(&nodeAddr);
+    static linkaddr_t nodeAddr;
+    nodeAddr.u8[0] = BODY_ADDR_A;
+    nodeAddr.u8[1] = BODY_ADDR_B;
+    linkaddr_set_node_addr(&nodeAddr);
 
-  process_start(&init_network_process, NULL);
+    process_start(&init_network_process, NULL);
 
-  PROCESS_END();
+    PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(init_network_process, ev, data)
-{
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
-  PROCESS_BEGIN();
+// Network init process
 
-  initMoveNetwork(&broadcast, &broadcast_call);
-  initGimbalNetwork(&broadcast, &broadcast_call);
+PROCESS_THREAD(init_network_process, ev, data) {
+    PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
-  PROCESS_END();
+    PROCESS_BEGIN();
+
+    initMoveNetwork(&broadcast, &broadcast_call);
+    initGimbalNetwork(&broadcast, &broadcast_call);
+
+    PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
+
