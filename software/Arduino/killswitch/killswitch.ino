a4eb2e76f3dcb0549c2a4e02f57018e43d9e1da1
Joshua Franz <jufranz@umich.edu>
2015-12-10 02:20:28 -0500

    fucked with shit

--- software/Arduino/killswitch/killswitch.ino
+++ software/Arduino/killswitch/killswitch.ino
@@ -23,7 +23,7 @@ void setup() {
   delay(10);
   Serial.println("Ready, Steady, Go");
   delay(10);
-    
+
   pinMode(ledPin, OUTPUT);
   pinMode(gatePin, OUTPUT);
   pinMode(faultPin, OUTPUT);
@@ -39,26 +39,26 @@ void setup() {
 void loop() {
   //val = analogRead(sensePin);
   boobs = parsePacket(MAX);
-  
+
   //Serial.println(val);
-  if(val > 600 && !fault) {
+  if (val > 600 && !fault) {
     enabled = false;
     fault = true;
   }
   //xbee manual kill portion
   //will kill if we haven't recieved a message for
   //MAX miliseconds, max at the top of the file
-  if(boobs == 3 || boobs == 1){
-      ok = false;
-      fault = true;
-    }
+  if (boobs == 3 || boobs == 1) {
+    ok = false;
+    fault = true;
+  }
   else
     ok = true;
 
   enabled = enabled && ok;
-  
+
   digitalWrite(ledPin, !enabled); //LED will be off when enabled
-  digitalWrite(faultPin, !fault); 
+  digitalWrite(faultPin, !fault);
   digitalWrite(gatePin, enabled);
 
   /*
@@ -67,14 +67,14 @@ void loop() {
    *    if fault is true, set fault to false, set button state to true
    *    if we've disabled power, set enable it, set enable to true, set button state to true
    *  otherwise set enable to false, set button state to tru
-   *  
+   *
    *Else set button state to false
    */
-  if(!digitalRead(buttonPin)) {
-    if(!btnState) {
-      if(fault) {
+  if (!digitalRead(buttonPin)) {
+    if (!btnState) {
+      if (fault) {
         fault = false;
-      } else if(!enabled) {
+      } else if (!enabled) {
         enabled = true;
         digitalWrite(gatePin, HIGH);
         delay(10);
@@ -87,16 +87,16 @@ void loop() {
     btnState = false;
   }
 
-  if(fault) {
+  if (fault) {
     int m = millis();
-    if(m % 1000 < 250) {
+    if (m % 1000 < 250) {
       tone(speakerPin, 5000);
-    } else if(m % 1000 < 500) {
+    } else if (m % 1000 < 500) {
       tone(speakerPin, 4000);
     } else {
       noTone(speakerPin);
     }
-    
+
   } else {
     noTone(speakerPin);
   }
